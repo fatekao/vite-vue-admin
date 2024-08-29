b6b7e3d08018a0612047d7c6e02bf65d37c7c8ad
Yfad@1123 <fatekao@qq.com>
2024-08-30 00:06:27 +0800

    update alias别名配置

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,17 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import VueSetupExtend from 'vite-plugin-vue-setup-extend'
 
+import { resolve } from 'path'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), VueSetupExtend()]
+  plugins: [vue(), vueJsx(), VueSetupExtend()],
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: resolve(__dirname, 'src')
+      }
+    ]
+  }
 })
