6e27de23c993d0b8aa00eb4ab2819a103d5926c1
Yfad@1123 <fatekao@qq.com>
2024-08-30 00:04:34 +0800

    update jsx和setup拓展

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import VueSetupExtend from 'vite-plugin-vue-setup-extend'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), vueJsx(), VueSetupExtend()]
 })
