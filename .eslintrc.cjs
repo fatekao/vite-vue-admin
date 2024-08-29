2fb90ab8a085f6567f2e7019719e69be889712a1
Yfad@1123 <fatekao@qq.com>
2024-08-29 23:59:56 +0800

    update eslint+prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: ['standard', 'plugin:vue/vue3-essential', 'prettier'],
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script'
+      }
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['vue', 'prettier'],
+  rules: {
+    'vue/multi-word-component-names': 'off',
+    'vue/no-useless-template-attributes': 'off',
+    'prettier/prettier': 'error'
+  }
+}
---
+++ .prettierrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  printWidth: 160, // 一行的字符数换行
+  tabWidth: 2, // 一个tab代表几个空格数
+  useTabs: false, // 是否使用tab进行缩进
+  singleQuote: true, // 字符串是否使用单引号
+  semi: false, // 行尾是否使用分号，默认为true
+  trailingComma: 'none', // 是否使用尾逗号
+  arrowParens: 'avoid', // 箭头函数单变量省略括号
+  bracketSpacing: true, // 对象大括号直接是否有空格，默认为true，效果：{ foo: bar }
+  singleAttributePerLine: false, // 是否每个属性单独占一行
+  endOfLine: 'auto' // 保留在 Windows 和 Unix 下的换行符
+}
